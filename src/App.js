670c51cc2fb82351d3df3eec4b896f8d344ccd74
Helen Evans <helenijevans@gmail.com>
2023-03-11 01:00:58 +0000

    netlify condition

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import About from "./Components/About";
 import Resume from "./Components/Resume";
-import Portfolio from "./Components/Portfolio";
+// import Portfolio from "./Components/Portfolio";
 
 class App extends Component {
   constructor(props) {
